9c91c2dac75b884e37d66ffce513b1de5c982fe0
algoives <jorge172@revature.net>
2023-10-21 01:11:22 -0700

    Create FeedItem module

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ives Murillo on 10/21/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
